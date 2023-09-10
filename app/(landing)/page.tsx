94719be7fad30159e15aceb9599b210084efe8a2
Muhammad Ahsan Farooq <ahsanfarooq210@gmail.com>
2023-09-10 11:27:27 +0500

    website competed

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,6 @@
+import LandingContent from "@/components/landing-content";
+import LandingHero from "@/components/landing-hero";
+import LandingNavbar from "@/components/landing-navbar";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
@@ -5,17 +8,11 @@ import React from "react";
 
 const LandingPage = () => {
   return (
-    <div>
-      Landing Page (unprotected)
-      <div>
-        <Link href="/sign-in">
-          <Button>Login</Button>
-        </Link>
-        <Link href="/sign-up">
-          <Button>Register</Button>
-        </Link>
-      </div>
-    </div>
+   <div className="h-full" >
+    <LandingNavbar/>
+    <LandingHero/>
+    <LandingContent/>
+   </div>
   );
 };
 
---
+++ components/landing-content.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
+const testimoniols = [
+  {
+    name: "Antonio",
+    avatar: "A",
+    title: "Software Engineer",
+    description: "This is the best application",
+  },
+  {
+    name: "Antonio",
+    avatar: "A",
+    title: "Software Engineer",
+    description: "This is the best application",
+  },
+  {
+    name: "Antonio",
+    avatar: "A",
+    title: "Software Engineer",
+    description: "This is the best application",
+  },
+  {
+    name: "Antonio",
+    avatar: "A",
+    title: "Software Engineer",
+    description: "This is the best application",
+  },
+  {
+    name: "Antonio",
+    avatar: "A",
+    title: "Software Engineer",
+    description: "This is the best application",
+  },
+  {
+    name: "Antonio",
+    avatar: "A",
+    title: "Software Engineer",
+    description: "This is the best application",
+  },
+  {
+    name: "Antonio",
+    avatar: "A",
+    title: "Software Engineer",
+    description: "This is the best application",
+  },
+];
+
+const LandingContent = () => {
+  return (
+    <div className="px-10 pb-20">
+      <h2 className="text-center text-white text-4xl font-extrabold mb-10">
+        Testimonials
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
+        {testimoniols.map((item, index) => {
+          return (
+            <Card
+              key={item.description}
+              className="bg-[#192339] border-none text-white ">
+              <CardHeader className="">
+                <CardTitle className=" flex items-center gap-x-2 ">
+                  <div>
+                    <p className="text-lg "> {item.name} </p>
+                    <p className="text-zinc-400 text-sm "> {item.title} </p>
+                  </div>
+                </CardTitle>
+                <CardContent className="pt-4 px-0">
+                  {item.description}
+                </CardContent>
+              </CardHeader>
+            </Card>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default LandingContent;
