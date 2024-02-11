ea4df69d0227d74923cc8c2e7181896864e26553
Muhammad Ahsan Farooq <ahsanfarooq210@gmail.com>
2024-02-11 13:06:07 +0500

    adding open ai embeddings

--- app/utils/LangchainWebLoader.ts
+++ app/utils/LangchainWebLoader.ts
@@ -1,5 +1,10 @@
 import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { createRetrievalChain } from "langchain/chains/retrieval";
 
 export const Initializelangchain = async () => {
   const loader = new CheerioWebBaseLoader(
@@ -12,4 +17,42 @@ export const Initializelangchain = async () => {
 
   const splitter = new RecursiveCharacterTextSplitter();
   const splitDocs = await splitter.splitDocuments(docs);
+  console.log(splitDocs.length);
+  console.log(splitDocs[0].pageContent.length);
+
+  const embeddings = new OpenAIEmbeddings();
+
+  const vectorstore = await MemoryVectorStore.fromDocuments(
+    splitDocs,
+    embeddings
+  );
+
+  const prompt =
+    ChatPromptTemplate.fromTemplate(`Answer the following question based only on the provided context:
+
+<context>
+{context}
+</context>
+
+Question: {input}`);
+  const chatModel = new ChatOpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
+  const documentChain = await createStuffDocumentsChain({
+    llm: chatModel,
+    prompt,
+  });
+
+  const retriever = vectorstore.asRetriever();
+
+  const retrievalChain = await createRetrievalChain({
+    combineDocsChain: documentChain,
+    retriever,
+  });
+
+  const result = await retrievalChain.invoke({
+    input: "what is LangSmith?",
+  });
+
+  console.log(result.answer);
 };
