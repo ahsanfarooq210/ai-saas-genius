1d07ac9e5f771000bb9e899a61e3fb0c8117a842
Muhammad Ahsan Farooq <ahsanfarooq210@gmail.com>
2026-03-16 04:21:55 +0500

    Remove fallback URL and simplify baseURL construction in axios client

--- front-end/src/lib/axios.ts
+++ front-end/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_URL || "http://localhost:5000/api",
+  baseURL: `${import.meta.env.VITE_SERVER_URL}/api`,
   withCredentials: true,
 });
 
